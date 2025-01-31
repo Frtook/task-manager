81f016c6fe1c996d939add1be80ec9758e29119f
Ibrahim_bin_ajjaj <119033743+Frtook@users.noreply.github.com>
2025-01-31 13:53:36 +0300

    fix json.parce error

--- components/Hydrated.tsx
+++ components/Hydrated.tsx
@@ -1,16 +1,29 @@
 "use client";
 import { useTaskStor } from "@/store/taskStor";
+import { useThemeStore } from "@/store/theme.store";
 import { useEffect, useState } from "react";
 
 export default function Hydrated({ children }: { children: React.ReactNode }) {
   const [isHydrated, setIsHydrated] = useState(false);
+  const { theme } = useThemeStore();
   useEffect(() => {
     (async () => {
       await useTaskStor.persist.rehydrate();
       setIsHydrated(true);
     })();
   }, []);
-  if (isHydrated) {
-    return <>{children}</>;
-  }
+  useEffect(() => {
+    (async () => {
+      await useTaskStor.persist.rehydrate();
+      const localTheme = JSON.parse(localStorage.getItem("theme") || "");
+      if (localTheme) {
+        document.documentElement.setAttribute(
+          "data-mode",
+          localTheme.state.theme
+        );
+        document.documentElement.className = localTheme.state.theme;
+      }
+    })();
+  }, [theme]);
+  return isHydrated ? <>{children}</> : null;
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,6 @@
 import logo from "@/public/vercel.svg";
 import { useThemeStore } from "@/store/theme.store";
 import Image from "next/image";
-import { useEffect } from "react";
 import { Sun, Moon } from "lucide-react";
 export default function Navbar() {
   const { setTheme, theme } = useThemeStore();
@@ -10,16 +9,6 @@ export default function Navbar() {
     setTheme();
   };
 
-  useEffect(() => {
-    const localTheme = JSON.parse(localStorage.getItem("theme") || "");
-    if (localTheme) {
-      document.documentElement.setAttribute(
-        "data-mode",
-        localTheme.state.theme
-      );
-      document.documentElement.className = localTheme.state.theme;
-    }
-  }, [theme]);
   return (
     <div className="flex gap-5 justify-between px-8 py-4 border-b border-b-black dark:border-gray-600">
       <Image src={logo} width={30} className="bg-[#0f172a]" alt="logo"></Image>
